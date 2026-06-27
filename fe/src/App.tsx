1995669531f610045ac60b4ea7b62d1436e4a9d0
krish-sehgal <krishsehgal08@gmail.com>
2026-06-27 18:37:35 +0530

    add:intialize-frontend

---
+++ fe/src/App.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import NotFound from "./pages/NotFound"
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
+
+export default App
